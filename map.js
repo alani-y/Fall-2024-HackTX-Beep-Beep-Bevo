2551805ffe6603d3d950661798c563b85e47fa11
Alani <alani.yu79@utexas.edu>
2024-11-02 18:59:38 -0500

    added map

--- map.js
+++ map.js
@@ -1,3 +1,8 @@
 
-// initializes a map and 
+
+// initializes a map and sets the starting coordinates
 var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
