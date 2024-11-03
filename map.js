be3709a5cffd1f0f9e3df503f3496d488dbabf04
Alani <alani.yu79@utexas.edu>
2024-11-02 19:28:50 -0500

    swapped map files

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 
 
 // initializes a map and sets the starting coordinates
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([30.284336, -97.734588], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
