558039bfb25d505902d7d4ab800d15af9ab42a80
Alani <alani.yu79@utexas.edu>
2024-11-30 19:58:59 -0500

    added pin deletion functionality

--- map.js
+++ map.js
@@ -31,6 +31,7 @@ window.addEventListener('resize', () => map.getViewPort().resize());
 var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
 // creates a marker at the Texas Union
+// Marker icon: https://icons.getbootstrap.com/
 var svg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
 </svg>`
@@ -50,13 +51,15 @@ function addMarkerPin(map){
     map.addEventListener("tap", function (evt) {
         var pin_coord = map.screenToGeo(evt.currentPointer.viewportX, evt.currentPointer.viewportY);
         var new_marker = new H.map.Marker({ lat: pin_coord.lat, lng: pin_coord.lng }, { icon: icon });
+
+        // adds an event listener to delete the pin if its tapped again
+        new_marker.addEventListener("longpress", function(){
+            map.removeObject(new_marker);
+        })
+
         map.addObject(new_marker)
     });
 }
 
-// adds a new icon when clicking on the map
-//document.getElementById('map').addEventListener("click", addMarkerPin(evt));
-
 addMarkerPin(map);
-
 
